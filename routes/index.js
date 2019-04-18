312ca0f8421e6a5a835c75db1512a80eb8efc99e
tran vu tuan <tranvutuan2001@gmail.com>
2019-04-18 17:03:14 +0200

    asd

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,13 @@ var ProductType = require("../models/productType");
 var Bill = require("../models/bill");
 /* GET home page. */
 
-router.post("/producttype", function (req, res, next) {
-    let productType = new ProductType({
-        type: "sinh to",
-    });
-    productType.save().then(() => {
-        res.send("success");
-    });
-});
 
 async function getProducts() {
     let res = await Product.find().populate('type').exec();
+    return res;
+}
+async function getProductType(){
+    let res = await ProductType.find().exec();
     return res;
 }
 
@@ -24,14 +20,9 @@ router.get("/products", function (req, res, next) {
         res.send(result);
     });
 });
-router.post("/", (req, res, next) => {
-    let product = new Product({
-        name: "dmm",
-        price: 7999,
-        type: "5cb09f78c9af1e1a748ef4fe"
-    });
-    product.save().then(() => {
-        res.json("success");
+router.get("/product-type", function (req,res,next){
+    getProductType().then(result=>{
+        res.send(result);
     })
 })
 router.post("/create-bill", (req, res, next) => {
